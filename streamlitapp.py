8ddceda58f1fc6ca83be8dc5f01066b98a5162ac
Kamakshi1306 <147923657+Kamakshi1306@users.noreply.github.com>
2023-12-13 23:39:48 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -12,13 +12,16 @@ def main():
     num2 = st.number_input("Enter the second number:", value=0, step=1)
     num3 = st.number_input("Enter the third number:", value=0, step=1)
 
-    # Calculate button
-    if st.button("Find"):
+    # Calculate button and Enter key handling
+    if st.button("Find") or st.session_state.enter_pressed:
         # Find the largest number
         result = find_largest_number(num1, num2, num3)
 
         # Display the result
         st.write(f"The largest number is: {result}")
+
+    # Listen for Enter key press
+    st.script("document.addEventListener('keydown', function(e) {if (e.key === 'Enter') {window.streamlit.sessionState.enter_pressed = true;}});")
 
 if __name__ == "__main__":
     main()
